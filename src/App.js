e5a6547c9f4a5d978755e8bce81e78980c4de25c
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-05-22 05:39:28 +0600

    daisy theme configuration

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 function App() {
   return (
-    <div >
-      <h2>Jantrik - Tools Manufacturer Website</h2>
+    <div className="text-3xl font-bold underline">
+      <h2 className="text-primary">Jantrik - Tools Manufacturer Website</h2>
+      <button class="btn btn-primary">One</button>
+      <button class="btn btn-secondary">Two</button>
+      <button class="btn btn-accent btn-outline">Three</button>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+
+          "primary": "#6419E6",
+          "secondary": "#D926A9",
+          "accent": "#1FB2A6",
+          "neutral": "#191D24",
+          "base-100": "#ffffff",
+          "info": "#3ABFF8",
+          "success": "#36D399",
+          "warning": "#FBBD23",
+          "error": "#F87272",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+}
