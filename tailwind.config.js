61091829066b8e7bbdced38e8645ea23633476f6
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-05-22 14:56:53 +0600

    theme color added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,22 +2,39 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  theme: {
+    extend: {},
+  },
   daisyui: {
     themes: [
       {
-        mytheme: {
-
-          "primary": "#6419E6",
-          "secondary": "#D926A9",
-          "accent": "#1FB2A6",
-          "neutral": "#191D24",
+        doctortheme: {
+          primary: "#0FCFEC",
+          secondary: "#19D3AE",
+          accent: "#3A4256",
+          neutral: "#3d4451",
           "base-100": "#ffffff",
-          "info": "#3ABFF8",
-          "success": "#36D399",
-          "warning": "#FBBD23",
-          "error": "#F87272",
         },
       },
+      "light",
+      "cupcake",
+    ],
+  }, theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        jantrik: {
+          primary: "#0FCFEC",
+          secondary: "#19D3AE",
+          accent: "#3A4256",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "light",
+      "cupcake",
     ],
   },
   plugins: [require('daisyui', '@tailwindcss/forms', '@tailwindcss/aspect-ratio')],
