83e7ef3bb9a52a04aab2b513fd04448b14c85c8e
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-05-22 06:07:19 +0600

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
+import Navbar from "./Components/Navbar";
+import logo from './Assets/Logo/logo.png';
+
 function App() {
   return (
-    <div className="text-3xl font-bold underline">
-      <h2 className="text-primary">Jantrik - Tools Manufacturer Website</h2>
-      <button class="btn btn-primary">One</button>
-      <button class="btn btn-secondary">Two</button>
-      <button class="btn btn-accent btn-outline">Three</button>
+    <div>
+      <Navbar logo={logo} />
     </div>
   );
 }
