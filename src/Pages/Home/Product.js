f97f7d4e1000e7af490ce16b36a7c10db4db69e6
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-05-22 22:32:57 +0600

    product description in home page

--- src/Pages/Home/Product.js
+++ src/Pages/Home/Product.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import MainButton from '../../Components/MainButton';
 
-const Product = () => {
+const Product = ({ item }) => {
+    const { _id, name, description, supplierName, price, stock, img } = item;
+
     return (
         <Link
-            to='/product/simple-watch'
+            to='/purchase'
             className='block'
         >
             <div className='aspect-w-1 aspect-h-1'>
@@ -12,18 +15,25 @@ const Product = () => {
                     loading='lazy'
                     alt='Simple Watch'
                     className='object-cover rounded'
-                    src='https://images.unsplash.com/photo-1523275335684-37898b6baf30'
+                    src={img}
                 />
             </div>
 
-            <div className='mt-2'>
-                <h5 className='font-medium'>
-                    Simple Watch
-                </h5>
+            <div className='mt-2 p-2'>
+                <div className='flex justify-between items-center mt-4'>
+                    <p className='text-sm text-gray-700'>${price}</p>
+                    <p className='text-sm text-gray-700'>Q {stock}</p>
+                </div>
 
-                <p className='mt-1 text-sm text-gray-700'>
-                    $150
-                </p>
+                <h5 className='font-medium mt-2'>{name?.length > 25 ? name.slice(0, 25) + ' ...' : name}</h5>
+
+                <p className='mt-1 text-sm'>{supplierName}</p>
+
+                <p className='mt-3'>{description?.length > 25 ? description.slice(0, 60) + ' ...' : description}</p>
+
+                <p className='mt-1 text-sm text-gray-700'>Minimum Order: {10}</p>
+
+                <MainButton design={'btn btn-sm btn-secondary text-white text-sm capitalize mt-5'} text={'Buy Now'} />
             </div>
         </Link>
     );
--- src/Pages/Home/Products.js
+++ src/Pages/Home/Products.js
@@ -1,8 +1,18 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import Loading from '../../Components/Loading';
 import Product from './Product';
 
 const Products = () => {
+    const [products, setProducts] = useState([]);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        fetch('https://posdash-server.herokuapp.com/inventory')
+            .then(res => res.json())
+            .then(data => setProducts(data));
+    }, []);
+
     return (
         <section>
             <div className='max-w-screen-xl px-4 py-12 mx-auto sm:px-6 lg:px-8'>
@@ -27,55 +37,9 @@ const Products = () => {
                     </div>
 
                     <div className='grid grid-cols-2 gap-4 lg:col-span-2 lg:grid-cols-3 lg:py-12'>
-                        <Product />
-
-                        <Link
-                            to='/product/simple-watch'
-                            className='block'
-                        >
-                            <div className='aspect-w-1 aspect-h-1'>
-                                <img
-                                    loading='lazy'
-                                    alt='Simple Watch'
-                                    className='object-cover rounded'
-                                    src='https://images.unsplash.com/photo-1523275335684-37898b6baf30'
-                                />
-                            </div>
-
-                            <div className='mt-2'>
-                                <h5 className='font-medium'>
-                                    Simple Watch
-                                </h5>
-
-                                <p className='mt-1 text-sm text-gray-700'>
-                                    $150
-                                </p>
-                            </div>
-                        </Link>
-
-                        <Link
-                            to='/product/simple-watch'
-                            className='block'
-                        >
-                            <div className='aspect-w-1 aspect-h-1'>
-                                <img
-                                    loading='lazy'
-                                    alt='Simple Watch'
-                                    className='object-cover rounded'
-                                    src='https://images.unsplash.com/photo-1523275335684-37898b6baf30'
-                                />
-                            </div>
-
-                            <div className='mt-2'>
-                                <h5 className='font-medium'>
-                                    Simple Watch
-                                </h5>
-
-                                <p className='mt-1 text-sm text-gray-700'>
-                                    $150
-                                </p>
-                            </div>
-                        </Link>
+                        {
+                            products?.slice(0, 3).map((item, index) => <Product key={index} item={item} />)
+                        }
                     </div>
                 </div>
             </div>
