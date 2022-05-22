df0a6d5b38c62fbc708d680fce66a76e4f22e5fc
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-05-22 12:13:10 +0600

    tabindex is tabIndex

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,10 +5,10 @@ const Navbar = ({ logo }) => {
         <div className='navbar bg-base-100'>
             <div className='navbar-start'>
                 <div className='dropdown'>
-                    <label tabindex='0' className='btn btn-ghost lg:hidden'>
+                    <label tabIndex='0' className='btn btn-ghost lg:hidden'>
                         <svg xmlns='http://www.w3.org/2000/svg' className='h-5 w-5' fill='none' viewBox='0 0 24 24' stroke='currentColor'><path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M4 6h16M4 12h8m-8 6h16' /></svg>
                     </label>
-                    <ul tabindex='0' className='menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52'>
+                    <ul tabIndex='0' className='menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52'>
                         <li><a>Item 1</a></li>
                         <li><a>Item 3</a></li>
                     </ul>
