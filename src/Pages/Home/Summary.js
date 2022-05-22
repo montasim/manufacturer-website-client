8c0cf04c16ab25f95c412b3fd765c79ca042d4f9
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-05-22 19:09:26 +0600

    summary design

--- src/Pages/Home/Summary.js
+++ src/Pages/Home/Summary.js
@@ -4,24 +4,24 @@ const Summary = () => {
     return (
         <section className='text-center'>
             <div className='max-w-screen-xl px-4 py-16 mx-auto sm:px-6 lg:px-8'>
-                <ul className='grid grid-cols-2 gap-4 border-2 border-teal-600 rounded-xl lg:grid-cols-4'>
+                <ul className='grid grid-cols-2 gap-4 lg:grid-cols-4'>
                     <li className='p-8'>
-                        <p className='text-2xl font-extrabold text-teal-500'>50</p>
-                        <p className='mt-1 text-lg font-medium'>Websites</p>
+                        <p className='text-3xl font-extrabold text-gray-800'>50</p>
+                        <p className='mt-1 text-lg font-medium'>Shop</p>
                     </li>
 
                     <li className='p-8'>
-                        <p className='text-2xl font-extrabold text-teal-500'>190k+</p>
-                        <p className='mt-1 text-lg font-medium'>Impressions</p>
+                        <p className='text-3xl font-extrabold text-gray-800'>190k+</p>
+                        <p className='mt-1 text-lg font-medium'>Users</p>
                     </li>
 
                     <li className='p-8'>
-                        <p className='text-2xl font-extrabold text-teal-500'>$150k+</p>
+                        <p className='text-3xl font-extrabold text-gray-800'>$150k+</p>
                         <p className='mt-1 text-lg font-medium'>Profits</p>
                     </li>
 
                     <li className='p-8'>
-                        <p className='text-2xl font-extrabold text-teal-500'>10+</p>
+                        <p className='text-3xl font-extrabold text-gray-800'>80+</p>
                         <p className='mt-1 text-lg font-medium'>Staff</p>
                     </li>
                 </ul>
