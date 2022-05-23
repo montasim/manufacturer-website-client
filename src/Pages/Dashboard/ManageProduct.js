a1b8ea624e1aa5dcf612b91a9497c5080c39eee3
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-05-23 16:25:11 +0600

    add item to cart

--- src/Pages/Dashboard/ManageProduct.js
+++ src/Pages/Dashboard/ManageProduct.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
-import { AiFillDelete } from 'react-icons/ai';
+import { AiFillDelete, AiFillEdit, AiFillPlusSquare } from 'react-icons/ai';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../Hooks/Firebase.Init';
 
-const ManageProduct = ({ index, product }) => {
-    const { _id, name, category, supplierName, img, price, inStock, shipping: shipping = 0 } = product;
+const Product = ({ index, product }) => {
+    const { _id, name, category, supplierName, img, description, price, stock, shipping } = product;
+    const navigate = useNavigate();
+    const [user] = useAuthState(auth);
 
-    const deleteMyItems = _id => {
+    let email;
+
+    if (user !== null) {
+        user.providerData.forEach((profile) => {
+            email = profile?.email;
+        });
+    }
+
+    const itemDelete = _id => {
         const confirm = window.confirm('Are You Sure?');
 
         if (confirm) {
@@ -19,7 +32,7 @@ const ManageProduct = ({ index, product }) => {
                         toast(
                             <div className='flex'>
                                 <img className='w-20' src={img} alt="" />
-                                <p className='ml-4'>{name} deleted from my items.</p>
+                                <p className='ml-4'>{name} deleted from inventory.</p>
                             </div>
                         );
                     }
@@ -27,14 +40,38 @@ const ManageProduct = ({ index, product }) => {
         }
     }
 
+    const addMyItems = () => {
+
+        const item = { name, category, supplierName, img, description, price, stock, shipping, email };
+
+        // send data to server
+        fetch('https://tools-manufacturer-server.herokuapp.com/add-my-cart', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(item)
+        })
+            .then(res => res.json())
+            .then(data => {
+                toast(
+                    <div className='flex'>
+                        <img className='w-20' src={img} alt="" />
+                        <p className='ml-4'>{name} added to my cart.</p>
+                    </div>
+                );
+            });
+
+    }
+
     return (
-        <tr className="border-b hover:bg-indigo-100 hover:text-grey-700">
+        <tr className="border-b hover:bg-indigo-100 hover:text-grey-700 justify-center">
             <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{index + 1}</td>
             <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
                 <img className='rounded-md w-16 lg:w-20' src={img} alt="" />
             </td>
             <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                {name.slice(0, 15)}
+                {name.length > 25 ? name.slice(0, 21) + '...' : name}
             </td>
             <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                 {category}
@@ -46,19 +83,21 @@ const ManageProduct = ({ index, product }) => {
                 $ {price}
             </td>
             <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                {inStock}
+                {stock}
             </td>
             <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                 {shipping}
             </td>
             <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                $ {inStock * price}
+                $ {stock * price}
             </td>
-            <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                <AiFillDelete onClick={() => deleteMyItems(_id)} className='text-2xl text-red-500 mr-3' />
+            <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap flex lg:mt-7 mt-2">
+                <AiFillEdit onClick={() => navigate(`/inventory/${_id}`)} className='text-2xl text-indigo-500 mr-3' />
+                <AiFillPlusSquare onClick={() => addMyItems(_id)} className='text-2xl text-gray-600 mr-3' />
+                <AiFillDelete onClick={() => itemDelete(_id)} className='text-2xl text-red-500 mr-3' />
             </td>
         </tr>
     );
 };
 
-export default ManageProduct;
+export default Product;
