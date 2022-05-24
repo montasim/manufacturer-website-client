85f2f6b9bfca94011b702818a6c5803edde7cb51
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-05-25 02:27:12 +0600

    404 page with gif

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import MainButton from '../../Components/MainButton';
+import e404 from '../../Assets/GIF/e404.gif';
 
 const NotFound = () => {
     const navigate = useNavigate();
 
     return (
-        <div className='flex flex-col h-screen items-center justify-center my-4'>
-            <h5 className='text-center text-xl text-gradient-to-r from-secondary to-primary w-full'>The Page You Are Looking Is</h5>
-            <h1 className='text-6xl text-red-500 uppercase font-bold my-4'>Not Found</h1>
-            <MainButton onClick={() => navigate('/')} design={'btn btn-active text-white uppercase mt-6'} text={'Back To Home'} />
+        <div className='flex flex-col h-screen items-center justify-center my-16'>
+            <img src={e404} alt="" />
+            <button onClick={() => navigate('/')} className='btn btn-active text-white uppercase'>Back to home</button>
         </div>
     );
 };
