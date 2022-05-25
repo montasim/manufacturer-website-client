1db2d82035651434a4e817f69819693332a97bfd
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-05-25 21:17:59 +0600

    add category

--- src/Pages/Dashboard/Categories/AddCategory.js
+++ src/Pages/Dashboard/Categories/AddCategory.js
@@ -35,13 +35,15 @@ const AddCategory = () => {
     return (
         <div>
             <form onSubmit={addCategory} className='flex h-screen flex-col items-center justify-center'>
-                <input type="text" placeholder="Category Image" className="input input-bordered input-secondary w-full max-w-xl" name='categoryName' required />
+                <h2 className='text-2xl text-bold mb-4'>Add A Category Here</h2>
 
-                <input type="text" placeholder="Category Image" className="input input-bordered input-secondary w-full max-w-xl" name='categoryImg' required />
+                <input type="text" placeholder="Category Name" className="input input-bordered input-secondary w-full max-w-xl" name='categoryName' required />
 
-                <textarea className="textarea textarea-secondary w-full max-w-xl my-4" placeholder="Description" name='categoryDescription' required></textarea>
+                <input type="text" placeholder="Category Image" className="input input-bordered input-secondary w-full max-w-xl my-4" name='categoryImg' required />
 
-                <button type='submit' className="btn btn-secondary btn-xs sm:btn-sm md:btn-md lg:btn-md max-w-xl text-white">Create Category</button>
+                <textarea className="textarea textarea-secondary w-full max-w-xl" placeholder="Description" name='categoryDescription' required></textarea>
+
+                <button type='submit' className="btn btn-secondary btn-xs sm:btn-sm md:btn-md lg:btn-md max-w-xl text-white mt-4">Create Category</button>
             </form>
         </div>
     );
