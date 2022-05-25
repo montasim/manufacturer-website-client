86000717db0ad52a8de66cedf420e5f76d159bb1
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-05-25 22:23:33 +0600

    sidebar icon

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -3,51 +3,124 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link, Outlet } from 'react-router-dom';
 import auth from '../../Hooks/Firebase.Init';
 import useAdmin from '../../Hooks/useAdmin';
+import { AiOutlineAppstoreAdd } from 'react-icons/ai';
+import { BiCategory, BiMessageSquareAdd } from 'react-icons/bi';
 import { CgUserAdd } from 'react-icons/cg';
 import { FaProductHunt } from 'react-icons/fa';
 import { FiSettings, FiUsers } from 'react-icons/fi';
 import { GoDiffAdded } from 'react-icons/go';
 import { GrUserAdmin } from 'react-icons/gr';
 import { HiOutlineShoppingBag } from 'react-icons/hi';
-import { MdOutlineRateReview } from 'react-icons/md';
-import { RiUserAddLine } from 'react-icons/ri';
+import { MdOutlineRateReview, MdAddComment } from 'react-icons/md';
+import { RiUserAddLine, RiAdminLine } from 'react-icons/ri';
+import { SiMicrodotblog } from 'react-icons/si';
 import defaultUserImage from '../../Assets/Images/defaultUserImage.png';
 
 const Dashboard = () => {
     const [user] = useAuthState(auth);
     const [admin] = useAdmin(user);
+    const todayDate = new Date();
+
     return (
         <div className="drawer drawer-mobile">
             <input id="dashboard-sidebar" type="checkbox" className="drawer-toggle" />
             <div className="drawer-content">
                 <Outlet></Outlet>
             </div>
-            <div className="drawer-side">
+            <div className="drawer-side shadow-xl">
                 <label htmlFor="dashboard-sidebar" className="drawer-overlay"></label>
                 <ul className="menu p-4 overflow-y-auto w-48 bg-base-100 text-base-content">
                     {
                         !admin && <>
-                            <li><Link to="/dashboard/my-orders">My Orders</Link></li>
-                            <li><Link to="/dashboard/my-blogs">My Blogs</Link></li>
+                            <li>
+                                <Link to="/dashboard/my-orders">
+                                    <HiOutlineShoppingBag className='text-secondary' />
+                                    My Orders
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/dashboard/my-blogs">
+                                    <SiMicrodotblog className='text-secondary' />
+                                    My Blogs
+                                </Link>
+                            </li>
                         </>
                     }
-                    <li><Link to="/dashboard/my-profile">My Profile</Link></li>
+                    <li>
+                        <Link to="/dashboard/my-profile">
+                            <FiSettings className='text-secondary' />
+                            My Profile
+                        </Link>
+                    </li>
                     {
                         !admin && <>
-                            <li><Link to="/dashboard/add-review">Add Review</Link></li>
-                            <li><Link to="/dashboard/add-blog">Add Blog</Link></li>
+                            <li>
+                                <Link to="/dashboard/add-review">
+                                    <BiMessageSquareAdd className='text-secondary' />
+                                    Add Review
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/dashboard/add-blog">
+                                    <MdAddComment className='text-secondary' />
+                                    Add Blog
+                                </Link>
+                            </li>
                         </>
                     }
                     {admin && <>
-                        <li><Link to="/dashboard/all-orders">All Orders</Link></li>
-                        <li><Link to="/dashboard/all-category">All Category</Link></li>
-                        <li><Link to="/dashboard/all-products">All Products</Link></li>
-                        <li><Link to="/dashboard/all-users">All Users</Link></li>
-                        <li><Link to="/dashboard/all-admin">All Admins</Link></li>
-                        <li><Link to="/dashboard/all-reviews">All Reviews</Link></li>
-                        <li><Link to="/dashboard/add-category">Add category</Link></li>
-                        <li><Link to="/dashboard/add-product">Add Product</Link></li>
-                        <li><Link to="/dashboard/add-admin">Add Admin</Link></li>
+                        <li>
+                            <Link to="/dashboard/all-orders">
+                                <HiOutlineShoppingBag className='text-secondary' />
+                                All Orders
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/dashboard/all-category">
+                                <BiCategory className='text-secondary' />
+                                All Category
+                            </Link>
+                        </li>
+                        <li><Link to="/dashboard/all-products">
+                            <FaProductHunt className='text-secondary' />
+                            All Products</Link>
+                        </li>
+                        <li>
+                            <Link to="/dashboard/all-users">
+                                <FiUsers className='text-secondary' />
+                                All Users
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/dashboard/all-admin">
+                                <RiAdminLine className='text-secondary' />
+                                All Admins
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/dashboard/all-reviews">
+                                <MdOutlineRateReview className='text-secondary' />
+                                All Reviews
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/dashboard/add-category">
+                                <AiOutlineAppstoreAdd className='text-secondary' />
+                                Add category
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/dashboard/add-product">
+                                <GoDiffAdded className='text-secondary' />
+                                Add Product
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/dashboard/add-admin">
+                                <CgUserAdd className='text-secondary' />
+                                Add Admin
+                            </Link>
+                        </li>
                     </>}
                 </ul>
             </div>
