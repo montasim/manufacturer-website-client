a1c69c2a07864296825f401128f00cd8ccc1e466
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-05-26 19:27:31 +0600

    in home page product card design

--- src/Pages/Home/Product.js
+++ src/Pages/Home/Product.js
@@ -1,64 +1,102 @@
 import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import auth from '../../Hooks/Firebase.Init';
+import { BsCartCheck } from 'react-icons/bs';
+import { MdOutlineDeliveryDining, MdBookmarkBorder } from 'react-icons/md';
+import { toast } from 'react-toastify';
 
 const Product = ({ item }) => {
     const { _id, productName, productCategory, productSellerName, productImg, productDescription, productPrice, productInStock, minOrder } = item;
     const [user] = useAuthState(auth);
     const email = user?.email;
+    const navigate = useNavigate();
+    const orderedQuantity = minOrder;
 
     const addToCart = (_id) => {
-        const orderedQuantity = minOrder;
 
-        if (orderedQuantity <= productInStock) {
-            const item = { productName, productCategory, productSellerName, productImg, productDescription, productPrice, productInStock, orderedQuantity, minOrder, email };
+        const item = { productName, productCategory, productSellerName, productImg, productDescription, productPrice, productInStock, orderedQuantity, minOrder, email };
 
-            // send data to server
-            fetch('https://tools-manufacturer-server.herokuapp.com/add-cart', {
-                method: 'POST',
-                headers: {
-                    'content-type': 'application/json'
-                },
-                body: JSON.stringify(item)
-            })
-                .then(res => res.json())
-                .then(data => {
-                });
-        };
+        // send data to server
+        fetch('https://tools-manufacturer-server.herokuapp.com/add-cart', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(item)
+        })
+            .then(res => res.json())
+            .then(data => {
+                toast.success(
+                    <div className="flex items-center space-x-1">
+                        <div className="avatar">
+                            <div className="mask mask-squircle w-12 h-12">
+                                <img src={productImg} alt={productName + 'img'} />
+                            </div>
+                        </div>
+                        <div>
+                            <div className="font-semibold">{productName}</div>
+                            <div className="text-sm opacity-50">Added to Cart</div>
+                        </div>
+                    </div>
+                );
+            });
     };
 
     return (
-        <Link
-            to='/cart'
-            className='block'
+        <div
+            className="block p-4 rounded-lg shadow-lg shadow-indigo-100 border border-gray-400"
         >
-            <div className='aspect-w-1 aspect-h-1'>
-                <img
-                    loading='lazy'
-                    alt='Simple Watch'
-                    className='object-cover rounded'
-                    src={productImg}
-                />
-            </div>
+            <img
+                alt={productName + 'image'}
+                src={productImg}
+                className="object-cover w-60 h-56 rounded-md"
+            />
 
-            <div className='mt-2 p-2'>
-                <div className='flex justify-between items-center mt-4'>
-                    <p className='text-sm text-gray-700'>${productPrice}</p>
-                    <p className='text-sm text-gray-700'>Q {productInStock}</p>
+            <div className="mt-4">
+                <div>
+                    <p className="text-sm text-gray-400">${productPrice}.00</p>
+                    <p className="font-medium">{productName}</p>
                 </div>
 
-                <h5 className='font-medium mt-2'>{productName?.length > 25 ? productName.slice(0, 25) + ' ...' : productName}</h5>
+                <dl className="flex items-center mt-6 space-x-8 text-xs">
+                    <div className="sm:inline-flex sm:items-center sm:shrink-0">
+                        <MdBookmarkBorder className='text-2xl text-secondary' />
 
-                <p className='mt-1 text-sm'>{productName}</p>
+                        <div className="sm:ml-3 mt-1.5 sm:mt-0">
+                            <dt className="text-gray-500">
+                                Min Order
+                            </dt>
 
-                <p className='mt-3'>{productDescription?.length > 25 ? productDescription.slice(0, 60) + ' ...' : productDescription}</p>
+                            <dd className="font-medium">
+                                {minOrder}
+                            </dd>
+                        </div>
+                    </div>
 
-                <p className='mt-1 text-sm text-gray-700'>Minimum Order: {minOrder}</p>
+                    <div className="sm:inline-flex sm:items-center sm:shrink-0">
+                        <MdOutlineDeliveryDining className='text-2xl text-secondary' />
 
-                <button onClick={() => addToCart(_id)} className='btn btn-sm btn-secondary text-white text-sm capitalize mt-5'>Buy Now</button>
+                        <div className="sm:ml-3 mt-1.5 sm:mt-0">
+                            <dt className="text-gray-500">
+                                Available
+                            </dt>
+
+                            <dd className="font-medium">
+                                {productInStock}
+                            </dd>
+                        </div>
+                    </div>
+                </dl>
+
+                <div className="flex justify-between items-center mt-6">
+                    <button onClick={() => {
+                        navigate(`/products/${_id}`)
+                    }} className="btn btn-secondary btn-sm text-white">Details</button>
+                    <BsCartCheck onClick={() => addToCart(_id)} className='text-2xl text-primary' />
+                </div>
             </div>
-        </Link>
+        </div>
     );
 };
 
