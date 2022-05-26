9480a34afe645a344ca18863b6deec8779ccfe18
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-05-26 17:17:18 +0600

    products page

--- src/Pages/Products/Products.js
+++ src/Pages/Products/Products.js
@@ -12,6 +12,17 @@ const Products = () => {
 
     return (
         <div>
+            <div className='flex items-center p-8  rounded'>
+                <div className='mx-auto text-center mt-10 sm:mt-4'>
+                    <h2 className='text-4xl font-bold text-secondary'>
+                        Our Products
+                    </h2>
+
+                    <p className='mt-4 text-sm text-gray-700 max-w-[85ch]'>
+                        Diagnostics, Inspection & Locating, Pressing, Drain Cleaning, Threading & Pipe Fabrication, Wrenches & Tubing Tools, Utility & Electricians Tools, General Purpose & Hand Tools, Wet/Dry Vacs, Power Tools
+                    </p>
+                </div>
+            </div>
             <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-1 gap-8 place-items-center my-20">
                 {
                     products.map((product, index) => <ProductRow key={index} product={product} index={index} />)
