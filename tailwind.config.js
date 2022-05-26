5e47727d8d627b4d9862c64e14561dba9c0fe16a
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-05-26 18:19:09 +0600

    site color changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,8 +26,8 @@ module.exports = {
     themes: [
       {
         jantrik: {
-          primary: "#0FCFEC",
-          secondary: "#19D3AE",
+          primary: "#ef4444",
+          secondary: "#e11d48",
           accent: "#3A4256",
           neutral: "#3d4451",
           "base-100": "#ffffff",
