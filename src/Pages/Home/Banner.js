6386e146879aee5937e28fe0ae3e176939d6a0b4
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-05-26 16:29:33 +0600

    banner

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -7,7 +7,7 @@ const Banner = () => {
         <section className='relative bg-white'>
             <img
                 className='absolute inset-0 object-[75%] sm:object-[25%] object-cover w-full h-full opacity-25 sm:opacity-100'
-                src='https://images.unsplash.com/photo-1601758003122-53c40e686a19'
+                src='https://i.ibb.co/zrCYLSG/17491-RIDGID-H001-APXmicro-Reel-Camera-1x-Desktop-1280x590.png'
                 alt='Couple on a bed with a dog'
             />
 
@@ -16,14 +16,14 @@ const Banner = () => {
             <div className='relative max-w-screen-xl px-4 py-32 mx-auto lg:h-screen lg:items-center lg:flex'>
                 <div className='max-w-xl text-center sm:text-left'>
                     <h1 className='text-3xl font-extrabold sm:text-5xl'>
-                        Understand User Flow.
+                        Get a Better Look
                         <strong className='font-extrabold text-primary sm:block'>
                             Increase Performance.
                         </strong>
                     </h1>
 
                     <p className='max-w-lg mt-4 sm:leading-relaxed sm:text-xl'>
-                        Feel the key Performance. Optimized view, blazing fast performance
+                        The RIDGID® SeeSnake® microReel™ APX™ with TruSense® Technology shows in-pipe visuals like you've never seen them before.
                     </p>
 
                     <div className='flex flex-wrap gap-4 mt-8 text-center'>
